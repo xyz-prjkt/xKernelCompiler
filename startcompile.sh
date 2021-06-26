32d7aa8190180e6dc4631673701fdab3fc14563a
Jody Yuantoro <i.xyzuan@gmail.com>
2021-06-26 10:38:10 +0700

    xKernelCompile: major update

--- startcompile.sh
+++ startcompile.sh
@@ -4,25 +4,29 @@
 
 # Main
 DEVICE_DEFCONFIG= # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_DIR= # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER=xyzuan # Change with your own name or else.
-export KBUILD_BUILD_HOST=xyzscape # Change with your own hostname.
+CLANG_ROOTDIR= # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER= # Change with your own name or else.
+export KBUILD_BUILD_HOST= # Change with your own hostname.
 
 # Checking environtment
 # Warning !! Dont Change anything there without known reason.
-echo xKernelCompiler rev1.0 | a property of xyzprjkt
+echo ================================================
+echo xKernelCompiler
+echo version : rev1.0 - gaspoll
+echo ================================================
 echo BUILDER NAME = ${KBUILD_BUILD_USER}
 echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
 echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
-echo CLANG_DIR = ${CLANG_DIR}
+echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
+echo ================================================
 
 # Compile
 # Warning !! Dont Change anything there without known reason.
 function compile() {
     make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
     make -j$(nproc) ARCH=arm64 O=out \
-                               CC=${CLANG_DIR}/bin/clang \
-                               CROSS_COMPILE=${CLANG_DIR}/bin/aarch64-linux-gnu- \
-                               CROSS_COMPILE_ARM32=${CLANG_DIR}/bin/arm-linux-gnueabi-
+                               CC=${CLANG_ROOTDIR}/bin/clang \
+                               CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
+                               CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 }
 compile
